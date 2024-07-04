ebc044cde4663219e5ffa011dc97d4eb84c0fd73
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-04 16:22:08 +0200

    Fixes and step 3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+const mainList = document.querySelector(".list");
+
 const paragraph1 = document.createElement("p");
 paragraph1.innerText = "Eat Mountain Of Ice Cream";
 
 const paragraph2 = document.createElement("p");
 paragraph2.innerText = "Fly To The Moon";
 
+document.body.appendChild(paragraph1)
+mainList.insertAdjacentElement("beforeend", paragraph2);
 
-
-
-
-document.body.appendChild(paragraph1);
-document.body.insertAdjacentElement("beforeend", paragraph2);
+console.log(mainList.innerHTML);
