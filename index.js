c5a3f3a474dc7ff1b7d995f8762ed23819971f55
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-04 16:28:45 +0200

    Step 4 through 6

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ paragraph1.innerText = "Eat Mountain Of Ice Cream";
 const paragraph2 = document.createElement("p");
 paragraph2.innerText = "Fly To The Moon";
 
-document.body.appendChild(paragraph1)
+mainList.insertAdjacentElement("beforeend", paragraph1)
 mainList.insertAdjacentElement("beforeend", paragraph2);
 
+const divString = "<div>This is a div element as a string</div>";
+
 console.log(mainList.innerHTML);
+
+//mainList.innerHTML = divString;
+
+//console.log(mainList.innerHTML);
+
+mainList.insertAdjacentHTML("beforeend", divString);
+console.log(mainList.innerHTML);
+
