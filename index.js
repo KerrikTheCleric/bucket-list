666ee31fa4917c246d3f6a944d03a8a59ca539b7
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-04 15:53:01 +0200

    Step 2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const paragraph = document.createElement("p");
-paragraph.innerText = "Eat Mountain Of Ice Cream";
-document.body.appendChild(paragraph);
+const paragraph1 = document.createElement("p");
+paragraph1.innerText = "Eat Mountain Of Ice Cream";
+
+const paragraph2 = document.createElement("p");
+paragraph2.innerText = "Fly To The Moon";
+
+
+
+
+
+document.body.appendChild(paragraph1);
+document.body.insertAdjacentElement("beforeend", paragraph2);
